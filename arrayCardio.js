68f0e0ff13f592cb2039b48c398365976a6e624e
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-04-14 22:34:28 -0400

    sort names alphabetically

--- arrayCardio.js
+++ arrayCardio.js
@@ -79,14 +79,20 @@
         // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
         // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-        const category = document.querySelector(".mw-category");
-        const links = [...category.querySelectorAll('a')];
-        const de = links
-                    .map(link => link.textContent)
-                    .filter(streetName => streetName.includes('de'));
+        // const category = document.querySelector(".mw-category");
+        // const links = [...category.querySelectorAll('a')];
+        // const de = links
+        //             .map(link => link.textContent)
+        //             .filter(streetName => streetName.includes('de'));
 
         // 7. sort Exercise
         // Sort the people alphabetically by last name
+        const alpha = people.sort(function(lastOne, nextOne){
+            const [alast, afirst] = lastOne.split(", ");
+            const [blast, bfirst] = nextOne.split(", ");
+            return alast > blast ? 1 : -1;
+        });
+        console.log(alpha);
     
         // 8. Reduce Exercise
         // Sum up the instances of each of these
